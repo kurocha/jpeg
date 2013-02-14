b64cf7acebdf82eb776570e5102ec8da456da316
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2013-02-15 12:06:29 +1300

    Updated to support teapot 0.6

--- teapot.rb
+++ teapot.rb
@@ -3,22 +3,22 @@
 #  This file is part of the "Teapot" project, and is released under the MIT license.
 #
 
-required_version "0.5"
+required_version "0.6"
 
 define_target "jpeg" do |target|
 	target.install do |environment|
-		environment.use in:(package.path + "jpeg-8d") do |config|
-			Commands.run("make", "clean") if File.exist? "Makefile"
-				
-			Commands.run("./configure",
-				"--prefix=#{config.install_prefix}",
-				"--disable-dependency-tracking",
-				"--enable-shared=no",
-				"--enable-static=yes",
-				*config.configure
-			)
-				
-			Commands.run("make", "install")
+		install_external(package.path, "jpeg-8d", environment) do |config, fresh|
+			if fresh
+				Commands.run!("./configure",
+					"--prefix=#{config.install_prefix}",
+					"--disable-dependency-tracking",
+					"--enable-shared=no",
+					"--enable-static=yes",
+					*config.configure
+				)
+			end
+			
+			Commands.make_install
 		end
 	end
 	
