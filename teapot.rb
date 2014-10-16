d511ce110eaf579d3792bb4bb6bf0a7f68489bf3
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2014-10-16 15:01:44 +1300

    Trying out libjpeg-turbo

--- teapot.rb
+++ teapot.rb
@@ -7,27 +7,17 @@ teapot_version "1.0"
 
 define_target "jpeg" do |target|
 	target.build do
-		source_files = Files::Directory.join(target.package.path, "jpeg-9a")
-		cache_prefix = Path.join(environment[:build_prefix], "jpeg-9a-#{environment.checksum}")
-		package_file = Path.join(environment[:install_prefix], "lib/libjpeg.a")
-		
-		copy source: source_files, prefix: cache_prefix
-		
-		configure prefix: cache_prefix do
-			run! "./configure",
-				"--prefix=#{environment[:install_prefix]}",
-				"--disable-dependency-tracking",
-				"--enable-shared=no",
-				"--enable-static=yes",
-				*environment[:configure],
-				chdir: cache_prefix
-		end
-		
-		make prefix: cache_prefix, package_files: [package_file]
+		source_files = Files::Directory.join(target.package.path, "libjpeg-turbo-1.3.90")
+		cache_prefix = Files::Directory.join(environment[:build_prefix], "libjpeg-turbo-1.3.90-#{environment.checksum}")
+		package_files = Path.join(environment[:install_prefix], "lib/pkgconfig/libjpeg-turbo13.pc")
+
+		cmake source: source_files, build_prefix: cache_prefix
+
+		make prefix: cache_prefix, package_files: package_files
 	end
 	
 	target.depends "Build/Files"
-	target.depends "Build/Make"
+	target.depends "Build/CMake"
 	
 	target.depends :platform
 	
