6c2dd969826cc7d15b51e2d045dfc8f7ed0f8970
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2016-05-02 21:55:06 +1200

    Move autoreconf into configure block to avoid running it unnecessarily.

--- teapot.rb
+++ teapot.rb
@@ -13,9 +13,9 @@ define_target "jpeg" do |target|
 		
 		copy source: source_files, prefix: cache_prefix
 		
-		run! "autoreconf", "-fiv", chdir: cache_prefix
-		
 		configure prefix: cache_prefix do
+			run! "autoreconf", "-fiv", chdir: cache_prefix
+			
 			run! "./configure",
 				"--prefix=#{environment[:install_prefix]}",
 				"--disable-dependency-tracking",
